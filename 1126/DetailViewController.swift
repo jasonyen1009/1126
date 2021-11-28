e734732e3c25113aff303e582b9f0ff61136dd83
Jason <jasonyen1009@gmail.com>
2021-11-28 23:18:26 +0800

    第一次更新

---
+++ 1126/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  1126
+//
+//  Created by Hong Cheng Yen on 2021/11/27.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.isModalInPresentation = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 1126/ViewController.swift
+++ 1126/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let str = "This is ag Very Long Label"
+        
+        
+        let nsString = str as NSString
+        if nsString.length >= 5 {
+            label.text = nsString.substring(with: NSRange(location: 0, length: nsString.length > 5 ? 5 : nsString.length))
+        }
+        
+    }
+    
+    private func presentModal() {
+        let detailViewController = DetailViewController()
+        let nav = UINavigationController(rootViewController: detailViewController)
+        // 1
+        nav.modalPresentationStyle = .pageSheet
+
+        
+        // 2
+        if let sheet = nav.sheetPresentationController {
+
+            // 3
+            sheet.detents = [.medium(), .large()]
+
+        }
+        // 4
+        present(nav, animated: true, completion: nil)
+
     }
 
 
+    @IBAction func nextpage(_ sender: Any) {
+        if let controller = storyboard?.instantiateViewController(identifier: "t1") {
+              controller.isModalInPresentation = false
+//              present(controller, animated: true, completion: nil)
+           }
+        button1.isHidden = false
+        button2.isHidden = true
+
+        button1.self.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        button1.self.layer.borderWidth = 1
+        button1.self.layer.cornerRadius = 5
+    }
+    @IBAction func ttttt(_ sender: Any) {
+        print("button1")
+    }
+    @IBAction func button2(_ sender: Any) {
+        print("button2")
+    }
+    @IBAction func test(_ sender: Any) {
+        print("{get")
+    }
 }
 
